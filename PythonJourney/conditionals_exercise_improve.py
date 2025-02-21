5bb522bda4afe240af35fbf5ebd4be74e6d4391f
Miroslav-Miro <miroslavmazalov@gmail.com>
2025-02-21 19:07:46 +0200

    Cosmetic changes

--- PythonJourney/conditionals_exercise_improve.py
+++
@@ -1,9 +0,0 @@
-def num_days(month):
-    if month in ["January", "March", "May", "July", "August", "October", "December"]:
-        return 31
-    elif month in ["April", "June", "September", "November"]:
-        return 30
-    elif month == "February":
-        return 28
-    else:
-        return None
--- PythonJourney/user_input_exercise.py
+++
@@ -1,4 +0,0 @@
-name = input("What's your name \n").capitalize()
-distance_km = float(input("What's your distance in km \n"))
-
-print(f'Greetings {name}, your distance in KM is {distance_km} and in miles is{distance_km * 0.621371}')
