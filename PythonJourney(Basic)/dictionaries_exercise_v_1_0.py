2e3f39ea9d947dc88ce419949a680d7d839e686e
Miroslav-Miro <miroslavmazalov@gmail.com>
2025-02-24 18:06:34 +0200

    Update the file PythonJourney(Basic)/dictionaries_exercise_v_1_0.py and fix bugs

--- PythonJourney(Basic)/dictionaries_exercise_v_1_0.py
+++ PythonJourney(Basic)/dictionaries_exercise_v_1_0.py
@@ -1,56 +1,60 @@
-freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
-antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
-pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
-player = {'inventory':[]}
+freelancers = {
+    "name": "freelancing Shop",
+    "brian": 70,
+    "black knight": 20,
+    "biccus diccus": 100,
+    "grim reaper": 500,
+    "minstrel": -15,
+}
+antiques = {
+    "name": "Antique Shop",
+    "french castle": 400,
+    "wooden grail": 3,
+    "scythe": 150,
+    "catapult": 75,
+    "german joke": 5,
+}
+pet_shop = {"name": "Pet Shop", "blue parrot": 10, "white rabbit": 5, "newt": 2}
+player = {"inventory": [], "gold": 1000}
+bill = 0
 
 freelancers = {k.lower(): v for k, v in freelancers.items()}
 
-# Print out the name of each shop and the items they sell
+# go in SHOP and buy 1 item
+# add itema to player's inventory
 for shop in (freelancers, antiques, pet_shop):
-    print(f'    Welcome to the: \n ---{shop['name']}---')
+    shop_name = shop["name"].title()
+    print(f"    Welcome to the: \n ---{shop_name}---")
     print()
-    print('We have the following items for sale:')
-    
+    print("We have the following items for sale:")
+    print(f"Your inventory: {player['inventory']}")
+    for item in shop:
+        if item != "name":
+            print(f"==[{item}], {shop[item]} gold pieces")
     while True:
-     for item in shop:
-        if item != 'name':
-            print(f'==[{item}], {shop[item]} gold pieces')
-     picked_item = input("Choose 1 item to buy: ")
-     picked_item = picked_item.lower()
-     if picked_item not in shop:
-         print('Sorry, we do not have that item')
-         break
-     else:
-        break
-        
-    player['inventory'].append(picked_item)
-    print(f'You purchased {picked_item} for {shop[picked_item]} gold pieces')
-    shop.pop(picked_item) 
+        picked_item = input("Choose 1 item to buy or type EXIT to leave the shop: ")
+        picked_item = picked_item.lower()
+        if picked_item == "exit":
+            print(f"You exited the {shop_name}")
+            print()
+            break
+        elif picked_item not in shop or picked_item == "name":
+            print("Sorry, we do not have that item")
+            print()
+            for item in shop:
+                if item != "name":
+                    print(f"==[{item}], {shop[item]} gold pieces")
+        else:
+            player["inventory"].append(picked_item)
+            player["gold"] -= abs(shop[picked_item])
+            bill += abs(shop[picked_item])
+            print(f"You purchased {picked_item.upper()} for {shop[picked_item]} gold pieces")
+            shop.pop(picked_item)
+            break
 
-print(f'You purchased the following items:{"".join(player["inventory"])}')
-
-
-    
-
-    
-
-
-    
-    
-
-    
-
-
-    
-
-
-
-    
-
-
-
-
-
-
-            
-            
+if len(player["inventory"]) > 0:
+    print(f'You purchased the following items: \n{"\n".join(player["inventory"])}')
+    print(f"Total cost {bill} gold pieces.")
+    print(f"Remaining gold {player['gold']} gold pieces.")
+else:
+    print("You did not purchase any items")
